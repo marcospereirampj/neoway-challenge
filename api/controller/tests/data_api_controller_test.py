3203621cec035d436030b58b6f54b59db4b6bb82
Marcos Pereira <marcospereira.mpj@gmail.com>
2018-05-25 00:17:47 -0300

    Updated swagger docs.

---
+++ api/controller/tests/data_api_controller_test.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""Test de CRUD Localization Domain"""
+
+import unittest
+
+
+class DataApiControllerTest(unittest.TestCase):
+    """Test Data Process Class"""
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    def retrieve_test(self):
+        pass
+
+    def restore_test(self):
+        pass
+
+    def update_test(self):
+        pass
